8040daaeb2fb181ff7791df50cee8fd750f39803
josiahaltschuler <josiah.altschuler@gmail.com>
2019-11-22 13:41:32 -0600

    genetic stocks select boxes now post to page to display a filtered table

--- js/form.js
+++ js/form.js
@@ -1,43 +1,41 @@
 jQuery(document).ready(function($) {
-  // $( "#table-select" ).change(function() {
-  //   if ($(this).val()==="genetic_stocks") {
-  //     $("#genetic-stocks-form-section").show()
-  //   } else {
-  //     $("#germplasm-form-section").hide()
-  //   }
-  // });
   $( "#table-select" ).change(function() {
     var url = $(this).val(); // get selected value
-    if (url) { // require a URL
-        window.location = 'http://wgrc.local?table=' + url; // redirect
-    }
-    return false;
-    // var url = $(this).val(); // get selected value
     
-    // if (url) { // require a URL
-    //     window.location = url; // redirect
-    // }
-    // return false;
-    // var your_selected_value = $('#table-select option:selected').val();
-    // console.log(your_selected_value);
-    // $.ajax({
-    //     type: "POST",
-    //     url: "http://wgrc.local/",
-    //     data: {duh:your_selected_value},
-    //     success: function(data) {
-    //         // if (data.selected == "germplasm") {
-    //           var content = $( data ).find( "#content" );
-    //             console.log(data.duh);
-    //         // } else {
-    //           // console.log(data);            }
-    //     }
+    if (url) { // require a URL
+      window.location = 'http://wgrc.local?table=' + url; // redirect
+    }
 
-    // $.post( "http://wgrc.local", { name: "John", time: "2pm" })
-    //   .done(function( data ) {
-    //     alert( "Data Loaded: " + data );
-    // });
+    return false;
   });
 
-
+  $( "#type-select" ).change(function() {
+    var url = $(this).val(); // get selected value
     
+    if (url) { // require a URL
+      window.location = 'http://wgrc.local?table=genetic_stocks&stock_type=' + url; // redirect
+    }
+
+    return false;
+  });
+
+  $( "#gene-select" ).change(function() {
+    var url = $(this).val(); // get selected value
+    
+    if (url) { // require a URL
+      window.location = 'http://wgrc.local?table=genetic_stocks&genes=' + url; // redirect
+    }
+
+    return false;
+  });
+
+  $( "#chromosome-select" ).change(function() {
+    var url = $(this).val(); // get selected value
+    
+    if (url) { // require a URL
+      window.location = 'http://wgrc.local?table=genetic_stocks&chromosome_of_interest=' + url; // redirect
+    }
+
+    return false;
+  });
 })
